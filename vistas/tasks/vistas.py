85f9f1be170919dd6bb655228b3262da6f830f1a
Javier Leon <ja.leon1874@uniandes.edu.co>
2022-10-24 00:03:59 -0500

    Update06: Consultar archivos cargados y convertirlos en los formatos correspondientes

--- vistas/tasks/vistas.py
+++ vistas/tasks/vistas.py
@@ -3,11 +3,10 @@ from datetime import datetime
 import os
 import os.path
 import glob
-from pydub import AudioSegment
 from time import sleep
 from wsgiref import validate
 from flask import request
-from flask_jwt_extended import jwt_required, create_access_token,get_jwt
+from flask_jwt_extended import jwt_required, create_access_token, get_jwt
 import jwt
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
@@ -28,7 +27,8 @@ class VistaTasks(Resource):
     def post(self):
         strJwtRequest = request.headers['Authorization']
         parseJtwData = strJwtRequest[7:]
-        jwtDecoded = jwt.decode(parseJtwData, options={"verify_signature": False})
+        jwtDecoded = jwt.decode(parseJtwData, options={
+                                "verify_signature": False})
         user_id = jwtDecoded['sub']
 
         fileName = request.files['fileName'].filename
@@ -37,13 +37,14 @@ class VistaTasks(Resource):
         name = [
             value for value in fileName.split(".")][1]
         task = Task(fileName=fileName, originalFormat=originalFormat,
-                    newFormat=request.form.get("newFormat"), status=Status.UPLOADED, date=datetime.now(), usuario_id = user_id)
+                    newFormat=request.form.get("newFormat"), status=Status.UPLOADED, date=datetime.now(), usuario_id=user_id)
         db.session.add(task)
         db.session.commit()
 
-        dir_name = 'C:\\Users\\USER\\Documents\\'
+        dir_name = 'C:\\audiofiles\\'
         f = request.files['fileName']
-        f.save(os.path.join(dir_name, name + '-' + str(task.id) + '.' + originalFormat))
+        f.save(os.path.join(dir_name, name + '-' +
+               str(task.id) + '.' + originalFormat))
 
         return "La tarea fue creada correctamente"
 
@@ -51,8 +52,9 @@ class VistaTasks(Resource):
     def get(self):
         return [task_schema.dump(ca) for ca in Task.query.all()]
 
+
 class VistaTask(Resource):
-    
+
     @jwt_required()
     def get(self, id):
         return task_schema.dump(Task.query.get_or_404(id))
@@ -61,7 +63,7 @@ class VistaTask(Resource):
     def put(self, id):
 
         task = Task.query.get_or_404(id)
-        
+
         if task.status == 2:
             oldname = task.fileName
             oldnewf = task.newFormat
@@ -74,9 +76,9 @@ class VistaTask(Resource):
             oldnewname = nombresolo+"."+oldnewformat
             newname = nombresolo+"."+newformat
 
-            task.status=Status.UPLOADED
+            task.status = Status.UPLOADED
             db.session.commit()
-            #obtener archivo
+            # obtener archivo
             songs = glob.glob("*."+oldnewformat)
             print(songs)
 
@@ -84,30 +86,30 @@ class VistaTask(Resource):
 
                 song = s.lower()
 
-                if(song == oldnewname):
-                #convertir archivo
-                    print("song",s)
+                if (song == oldnewname):
+                    # convertir archivo
+                    print("song", s)
                     dst = newname
 
-                    print(song," y ",dst)
+                    print(song, " y ", dst)
 
                     # absolute_path = os.path.dirname(__file__)
                     # print(absolute_path)
 
                     # p = open("audio.mp3")
                     # print("hola",p.re)
-                                    
+
                     #sound = AudioSegment.from_file(absolute_path+"\\"+song, format=oldnewformat)
                     #sound = AudioSegment.from_mp3(absolute_path+"\\audio.mp3")
-                    #print(sound)
+                    # print(sound)
                     #sound.export(dst, format=newformat)
 
-                    task.newFormat=request.form.get("newFormat")
-                    task.status=Status.PROCESSED 
-                    task.date=datetime.now()
+                    task.newFormat = request.form.get("newFormat")
+                    task.status = Status.PROCESSED
+                    task.date = datetime.now()
                     db.session.commit()
 
-                #eliminar archivo anterior convertido
+                # eliminar archivo anterior convertido
 
                     return "El archivo fue modificado correctamente"
 
@@ -126,38 +128,39 @@ class VistaTask(Resource):
         strJwtRequest = request.headers['Authorization']
         parseJtwData = strJwtRequest[7:]
 
-        jwtDecoded = jwt.decode(parseJtwData, options={"verify_signature": False})
+        jwtDecoded = jwt.decode(parseJtwData, options={
+                                "verify_signature": False})
         user_id = jwtDecoded['sub']
 
         userTasks = Task.query.filter_by(usuario_id=user_id).all()
 
-        if userTasks:            
+        if userTasks:
             userTask = Task.query.filter_by(id=id_tarea).first()
             if userTask:
-                if userTask.status==2:        
+                if userTask.status == 2:
                     db.session.delete(userTask)
                     db.session.commit()
                     return 'Tarea eliminada con exito', 202
                 else:
-                    return 'La tarea no tiene estado PROCESSED', 404                
+                    return 'La tarea no tiene estado PROCESSED', 404
             else:
-                return "El id {} de la tarea, NO existe para el usuario {}".format(id_tarea,user_id)
+                return "El id {} de la tarea, NO existe para el usuario {}".format(id_tarea, user_id)
         else:
             return "El usuario NO tiene tareas creadas", 404
+
 
 class VistaFileProcessedByUser(Resource):
     @jwt_required()
     def get(self, id_user):
-        
+
         userTasks = Task.query.filter_by(usuario_id=id_user).all()
 
         updatedFiles = []
         processedFiles = []
 
-
         dictFiles = {
             "listFilesUploaded": [],
-            "listFilesProcessed":[]
+            "listFilesProcessed": []
         }
 
         if userTasks:
@@ -167,21 +170,28 @@ class VistaFileProcessedByUser(Resource):
                     dictFiles["listFilesUploaded"].append(userTask.fileName)
                 if userTask.status == 2:
                     dictFiles["listFilesProcessed"].append(userTask.fileName)
-           
+
             return json.dumps(dictFiles)
 
         else:
             return "El usuario no tiene documentos almacenados", 404
 
-class VistaFiles(Resource):
-    #@jwt_required()
-    def post(self):
-        tasks = Task.query.filter_by(status=id_tarea).first()
-        """ dir_name = 'C:\\Users\\USER\\Documents\\audio.mp3'
-        f = open(dir_name, "w") """
 
-        #make a copy of the invoice to work with
-        src="C:\\Users\\USER\\Documents\\audio.mp3"
-        dst="C:\\Users\\USER\\Documents\\audio.wav"
-        shutil.copy(src,dst)
-        
+class VistaFiles(Resource):
+    @jwt_required()
+    def post(self):
+        tasks = Task.query.filter_by(status="UPLOADED").all()
+        print(tasks)
+        for task in tasks:
+            originalFormat = [value for value in task.fileName.split(".")][-1]
+            name = [value for value in task.fileName.split(".")][1]
+            origen = 'C:\\audiofiles\\' + name + '-' + \
+                str(task.id) + '.' + originalFormat
+            print(origen)
+            destino = 'C:\\audiofiles\\' + name + '-' + \
+                str(task.id) + '.' + task.newFormat
+            print(destino)
+            shutil.copy(origen, destino)
+
+            task.status = Status.PROCESSED
+            db.session.commit()
